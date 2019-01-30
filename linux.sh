4c8a5b494bc4b396885397e00ba4bee1737fdb76
JGAntunes <me@jgantunes.com>
2019-01-30 19:25:52 +0000

    feat: store relevant ppas

--- linux.sh
+++ linux.sh
@@ -3,8 +3,12 @@
 fish_path=/usr/bin/fish
 
 install_from_dpkg_list () {
+  # Add PPAs
+  <tilde/.ppa.list xargs -I % sudo add-apt-repository %
+  # Install missing packages
   sudo apt update
-  sudo dpkg --set-selections < tilde/.dpkg.list
+  sudo cat tilde/.dpkg.list | dpkg --set-selections
+  sudo apt-get dselect-upgrade
 }
 
 install_from_dpkg_list
