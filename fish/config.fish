60fa660c145dc10649fe506077a643d6d03296a9
JGAntunes <me@jgantunes.com>
2019-09-17 11:38:18 +0100

    fix(fish): post exec hook for both macos and linux on installs

--- fish/config.fish
+++ fish/config.fish
@@ -41,61 +41,67 @@ test -x (which aws_completer);\
   and complete --command aws --no-files --arguments\
   '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
-# postexec hook for brew to update brewfile
+# postexec hook to update dependency lists
 function postexec --on-event fish_postexec
   if test (count $argv) -ge 1; and test $IS_OSX -eq 1
-    if string match -q -- "*brew install*" $argv;\
-      or string match -q -- "*brew cask install*" $argv;\
-      or string match -q -- "*brew remove*" $argv;
+    updateBrewfile $argv
+  end
+  if test (count $argv) -ge 1; and test $IS_LINUX -eq 1
+    updateDpkgList $argv
+  end
+end
 
-      brew bundle dump --force --global
+# update the brewfile
+function updateBrewfile
+  if string match -q -- "*brew install*" $argv;\
+    or string match -q -- "*brew cask install*" $argv;\
+    or string match -q -- "*brew remove*" $argv;
 
-      pushd $DOTFILES
-      set files (git diff --name-only)
+    brew bundle dump --force --global
 
-      if test (count $files) -eq 1; and set -l index (contains -i -- 'tilde/.Brewfile' $files)
-        read -l -P 'Commit Brewfile? [y/N] ' confirm
-        if test $confirm = 'Y' -o $confirm = 'y' -o $confirm = 'yes';
-          set -q DOTFILES_MSG; or set DOTFILES_MSG 'Updated Brewfile :beer:'
-          git commit -am $DOTFILES_MSG
-          git push
-        end
+    pushd $DOTFILES
+    set files (git diff --name-only)
+
+    if test (count $files) -eq 1; and set -l index (contains -i -- 'tilde/.Brewfile' $files)
+      read -l -P 'Commit Brewfile? [y/N] ' confirm
+      if test $confirm = 'Y' -o $confirm = 'y' -o $confirm = 'yes';
+        set -q DOTFILES_MSG; or set DOTFILES_MSG 'Updated Brewfile :beer:'
+        git commit -am $DOTFILES_MSG
+        git push
       end
-      popd
     end
+    popd
   end
 end
 
 
-# postexec hook for apt to update dpkg list
-function postexec --on-event fish_postexec
-  if test (count $argv) -ge 1; and test $IS_LINUX -eq 1
-    if string match -q -- "*apt install*" $argv;\
-      or string match -q -- "*apt-get install*" $argv;
+# update dpkg list
+function updateDpkgList
+  if string match -q -- "*apt install*" $argv;\
+    or string match -q -- "*apt-get install*" $argv;
 
-      # get the installed software
-      set installed (string split  -m1 -r ' ' (string trim $argv))
+    # get the installed software
+    set installed (string split  -m1 -r ' ' (string trim $argv))
 
-      # confirm we want to submit installed software to dpkg list
-      read -l -P 'Store in dpkg list file? [y/N] ' confirm
-      if test $confirm = 'Y' -o $confirm = 'y' -o $confirm = 'yes';
-        pushd $DOTFILES
-        echo "$installed install" >> ./tilde/.dpkg.list
+    # confirm we want to submit installed software to dpkg list
+    read -l -P 'Store in dpkg list file? [y/N] ' confirm
+    if test $confirm = 'Y' -o $confirm = 'y' -o $confirm = 'yes';
+      pushd $DOTFILES
+      echo "$installed install" >> ./tilde/.dpkg.list
 
-        # commit the changes
-        set files (git diff --name-only)
-        if test (count $files) -eq 1; and set -l index (contains -i -- 'tilde/.dpkg.list' $files)
-          read -l -P 'Commit dpkg list? [y/N] ' confirm
-          if test $confirm = 'Y' -o $confirm = 'y' -o $confirm = 'yes';
-            if test (count $files) -eq 1; and set -l index (contains -i -- 'tilde/.dpkg.list' $files)
-              set -q DOTFILES_MSG; or set DOTFILES_MSG "Updated dpkg list with $installed"
-              git commit -am $DOTFILES_MSG
-              git push
-            end
+      # commit the changes
+      set files (git diff --name-only)
+      if test (count $files) -eq 1; and set -l index (contains -i -- 'tilde/.dpkg.list' $files)
+        read -l -P 'Commit dpkg list? [y/N] ' confirm
+        if test $confirm = 'Y' -o $confirm = 'y' -o $confirm = 'yes';
+          if test (count $files) -eq 1; and set -l index (contains -i -- 'tilde/.dpkg.list' $files)
+            set -q DOTFILES_MSG; or set DOTFILES_MSG "Updated dpkg list with $installed"
+            git commit -am $DOTFILES_MSG
+            git push
           end
         end
-        popd
       end
+      popd
     end
   end
 end
