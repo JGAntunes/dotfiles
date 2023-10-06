713837a36ed7526662ff4b981660e8dbe455e417
JGAntunes <me@jgantunes.com>
2023-10-06 23:33:51 +0100

    fix: rgpg-stubs alias

--- fish/alias.fish
+++ fish/alias.fish
@@ -4,4 +4,4 @@ alias rgpg "gpg-connect-agent updatestartuptty /bye"
 
 # force gpg-agent to scan the card and recreate the key stubs
 # use this whenever you're inserting a different key with the same set of gpg-keys
-alias rgpg-stubs "gpg-connect-agent \"scd serialno" "learn --force\" /bye"
+alias rgpg-stubs "gpg-connect-agent \"scd serialno\" \"learn --force\" /bye"
