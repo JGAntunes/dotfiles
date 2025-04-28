139ef776ac0dc2d339753250a88f921de73f92be
JGAntunes <me@jgantunes.com>
2025-04-28 08:50:39 +0100

    feat(nvim): which key triggers

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,6 +1,11 @@
 return {
   "folke/which-key.nvim",
   opts = {
+    triggers = {
+      { "<auto>", mode = "nixsotc" },
+      { "<leader>", mode = "nixsotc" },
+      -- { "<C->", mode = "nixsotc" }, can't make this work to show CTRL mappings
+    },
     win = {
       border = "rounded",
     },
