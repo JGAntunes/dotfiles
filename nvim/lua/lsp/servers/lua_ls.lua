be91606cc81b06923c09b9e1ce3c65d8a59ea4ba
JGAntunes <me@jgantunes.com>
2025-04-29 10:53:46 +0100

    feat(nvim): blink completions

--- nvim/lua/lsp/servers/lua_ls.lua
+++ nvim/lua/lsp/servers/lua_ls.lua
@@ -1,12 +1,21 @@
 local lsp = require("lsp.shared-config")
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy"
+
 return {
   on_attach = lsp.on_attach,
   settings = {
     Lua = {
-      diagnostics = {
-        globals = { "vim" }, -- supress the annoying global warnings
+      diagnostics = { globals = { "vim" } }, -- supress the annoying global warnings
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          vim.fn.stdpath("config") .. "/lua", -- this line makes sure we have completions for our local plugins
+          lazypath, -- this line adds lazy plugins
+        },
       },
+      telemetry = { enable = false },
     },
   },
 }
